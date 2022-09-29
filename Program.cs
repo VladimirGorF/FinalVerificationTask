86b87f130d13fb4d04c46a18e62c1b52b8d8e07c
Vladimir Gorbunov <110591063+VladimirGorF@users.noreply.github.com>
2022-09-29 10:59:50 +0300

    Creating of cycle to fill NewArray, with Length condition < 4

--- Program.cs
+++ Program.cs
@@ -1,18 +1,25 @@
 ﻿using System.Linq;
 
 
-string [] Array = {"Moscow", "Tor", "Saint-Petersburg", "Vasilek", "23"};
+string [] Array = {"Moscow", "Tor", "Saint-Petersburg", "Vasilek", "23", "Z", "Bur"};
 int n = 0;
-string [] NewArray = new string [n];
 
 for (int i = 0; i < Array.Length; i++)
 {
     if(Array[i].Length < 4)
     {
         n++;
-    
-        // NewArray [i] = Array[i];
-        // Console.WriteLine(NewArray[i]);
     }
 }
-Console.WriteLine(n);
+
+string [] NewArray = new string [n];
+int j = 0;
+for (int i = 0; i < Array.Length; i++)
+{
+    if(Array[i].Length < 4)
+    {
+        NewArray [j] = Array[i];
+        Console.WriteLine(NewArray[j]);
+        j++;
+    } 
+}
