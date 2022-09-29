55c4a2bf295a759d35d8d5bfc8e22e9f76e25a98
Vladimir Gorbunov <110591063+VladimirGorF@users.noreply.github.com>
2022-09-29 10:15:13 +0300

    Creating new Array and cycl for counting of new Array Length

--- Program.cs
+++ Program.cs
@@ -1,1 +1,18 @@
-﻿string [] Array = {"Moscow", "Tor", "Saint-Petersburg", "Vasilek", "23"};
+﻿using System.Linq;
+
+
+string [] Array = {"Moscow", "Tor", "Saint-Petersburg", "Vasilek", "23"};
+int n = 0;
+string [] NewArray = new string [n];
+
+for (int i = 0; i < Array.Length; i++)
+{
+    if(Array[i].Length < 4)
+    {
+        n++;
+    
+        // NewArray [i] = Array[i];
+        // Console.WriteLine(NewArray[i]);
+    }
+}
+Console.WriteLine(n);
