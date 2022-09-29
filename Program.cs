939a559acf824513dc8fc281290fc1816c35810f
Vladimir Gorbunov <110591063+VladimirGorF@users.noreply.github.com>
2022-09-29 11:22:26 +0300

    Using function in Filling and create PrintArray function

--- Program.cs
+++ Program.cs
@@ -1,25 +1,36 @@
-﻿using System.Linq;
-
-
-string [] Array = {"Moscow", "Tor", "Saint-Petersburg", "Vasilek", "23", "Z", "Bur"};
+﻿string[] Array = { "Moscow", "Tor", "Saint-Petersburg", "Vasilek", "23", "Z", "Bur" };
 int n = 0;
 
 for (int i = 0; i < Array.Length; i++)
 {
-    if(Array[i].Length < 4)
+    if (Array[i].Length < 4) n++;
+}
+
+string[] NewArray = new string[n];
+void FillNewArray()
+{
+    int j = 0;
+    for (int i = 0; i < Array.Length; i++)
     {
-        n++;
+        if (Array[i].Length < 4)
+        {
+            NewArray[j] = Array[i];
+            j++;
+        }
     }
 }
 
-string [] NewArray = new string [n];
-int j = 0;
-for (int i = 0; i < Array.Length; i++)
+void PrintNewArray()
 {
-    if(Array[i].Length < 4)
+    for (int i = 0; i < NewArray.Length; i++)
     {
-        NewArray [j] = Array[i];
-        Console.WriteLine(NewArray[j]);
-        j++;
-    } 
+        if (i < NewArray.Length - 1) Console.Write($"{NewArray[i]}, ");
+        else Console.Write(NewArray[i]);
+    }
+    Console.WriteLine();
 }
+
+FillNewArray();
+PrintNewArray();
+
+
